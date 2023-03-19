8380e46fd4b7ac1cb29f27a000441875c21e7ced
chrysoberyl1460 <ehdwn6858@gmail.com>
2023-03-19 14:27:49 +0900

    removed Hello.java and added MenuManager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,11 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Orders");
+		System.out.println("2. Delete Orders");
+		System.out.println("3. Edit Orders");
+		System.out.println("4. View Orders");
+		System.out.println("5. Exit");
+	}
+}
